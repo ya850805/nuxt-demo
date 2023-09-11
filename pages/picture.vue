cc35f3d8481b05ad05eb70797f2a97034017599d
Jason <ya850805@gmail.com>
2023-09-12 01:56:03 +0800

    feat: Add static resource demo.

---
+++ pages/picture.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <img src="~/assets/test.jpeg">
+    (靜態資源放在assets目錄底下，以~開頭) <br>
+    <img src="/favicon.ico">
+    (靜態資源放在public目錄底下，以/開頭)
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+
+</style>
