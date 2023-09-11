38f59025b711d0151986c3e0a37957608cdf888d
Jason <ya850805@gmail.com>
2023-09-12 02:14:05 +0800

    feat: Add useHead demo.

---
+++ pages/head.vue
@@ -0,0 +1,32 @@
+<template>
+  <Title>這是Title(使用標籤最優先)</Title>
+  <h1>This is head demo pages</h1>
+</template>
+
+<script setup lang="ts">
+useHead({
+  title: 'This is title',
+  meta: [
+    { name: 'description', content: 'meta description' },
+    { name: 'keyword', content: 'meta keyword' }
+  ],
+  titleTemplate: (titleChunk) => {
+    return titleChunk ? `${titleChunk} - Site Title` : 'Site Title';
+  },
+  bodyAttrs: {
+    class: 'test'
+  },
+  script: [
+    {
+      src: '/a.js',
+      tagPosition: 'bodyClose'
+    }
+  ]
+})
+</script>
+
+<style>
+.test {
+  color: red
+}
+</style>
---
+++ public/a.js
@@ -0,0 +1,1 @@
+console.log('這是a.js的內容')
